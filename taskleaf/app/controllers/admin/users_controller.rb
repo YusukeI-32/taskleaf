fb86ccb3753b81ac1cb88af44eea7a2762fa41ac
Yusuke <now.good.1206@gmail.com>
2021-02-01 11:46:02 +0900

    進捗

--- taskleaf/app/controllers/admin/users_controller.rb
+++ taskleaf/app/controllers/admin/users_controller.rb
@@ -1,7 +1,21 @@
 class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
 
   def create
     @user = User.new(user_params)
@@ -11,11 +25,28 @@ class Admin::UsersController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+
+      if @user.update(params[:id])
+        redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました"
+      else
+        render :edit
+      end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました"
+  end
+  
   
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
-  
+
 end
